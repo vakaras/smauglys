25deb70438ac2d271dc6495331839fcb406facc7
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-08-07 19:26:27 +0200

    Try fixing the download failure.

---
+++ download.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+curl https://code.visualstudio.com/sha/download?build=stable&os=win32-x64 -Lo VSCodeSetup.exe
+curl https://www.python.org/ftp/python/3.8.10/python-3.8.10-amd64.exe -Lo PythonInstaller.exe
