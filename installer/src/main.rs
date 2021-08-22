46e5a8ba2dbf703c51f3e4657b003df166699e33
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-08-22 17:48:19 +0200

    Fix compilation errors.

--- installer/src/main.rs
+++ installer/src/main.rs
@@ -50,6 +50,12 @@ enum Error {
     },
 }
 
+impl From<std::io::Error> for Error {
+    fn from(error: std::io::Error) -> Self {
+        Self::IoError(error)
+    }
+}
+
 impl Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
