28ed4c3754c9170b55211a1e3b28fc283885e2dd
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-08-22 18:46:14 +0200

    Randomly trying to fix mt.exe.

--- build_installer.sh
+++ build_installer.sh
@@ -14,5 +14,6 @@ cp target/release/installer.exe ../smauglys.exe
 cd ..
 
 # Tell Windows that we need administrator priviliges.
-ls *
+ls -lh ./tools/mt.exe
+chmod 755 ./tools/mt.exe
 ./tools/mt.exe  -manifest "smauglys.exe.manifest" -outputresource:"smauglys.exe";1
