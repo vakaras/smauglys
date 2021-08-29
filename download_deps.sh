696da5e01f4dfd6d2919c584eb3990f67682ed85
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-08-29 15:59:41 +0200

    Try to use Python3.8 for downloading packages.

--- download_deps.sh
+++ download_deps.sh
@@ -18,7 +18,14 @@ curl 'https://marketplace.visualstudio.com/_apis/public/gallery/publishers/hedie
 # Note: We have to use 3.8 because it is the latest version that is
 # still supported on Windows 7.
 curl 'https://www.python.org/ftp/python/3.8.10/python-3.8.10-amd64.exe' -Lo PythonInstaller.exe
+# Since we need to use exactly the same Python and pip for downloading,
+# we install the downloaded Python on the builder.
+./PythonInstaller.exe /passive InstallAllUsers=1 PrependPath=1
+ls "$PROGRAMFILES"
+PYTHON_EXE="$PROGRAMFILES/Python38/python.exe"
+ls "$PYTHON_EXE"
+
 mkdir python_packages
 cd python_packages
 cp ../python-requirements.txt requirements.txt
-pip download -r requirements.txt
+"$PYTHON_EXE" -m pip download -r requirements.txt
