bcbec1333fba795a972da4ad25e4c46fbf72a4f0
Vytautas Astrauskas <vastrauskas@gmail.com>
2021-08-29 22:40:02 +0200

    Extend explanation.

--- installer/src/gui.rs
+++ installer/src/gui.rs
@@ -169,6 +169,11 @@ impl InstallerApp {
     }
     fn show_initial_wiew(&self) {
         trace!("[enter] show_initial_wiew");
+        self.explanation.set_text(concat!(
+            "Smauglys yra diegimo programa, kuri į Jūsų kompiuterį įdiegs: ",
+            "Python 3 su mypy bei pylint ir ",
+            "VS Codium su lietuvybės paketą bei Microsoft Python papildiniu.",
+        ));
         self.set_visible_initial_view(true);
         self.set_visible_progress_view(false);
         self.set_visible_final_wiew(false);
