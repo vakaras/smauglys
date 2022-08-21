abc2ec1fdda505493e8cc7f30fab5bb95d354568
Vytautas Astrauskas <vastrauskas@gmail.com>
2022-08-21 14:55:20 +0200

    Update VS Codium and dependencies.

--- download_deps.sh
+++ download_deps.sh
@@ -21,8 +21,8 @@ set -e
 set -v
 
 # Download VS Codium
-curl 'https://github.com/vakaras/smauglys-ide/releases/download/1.59.1/SmauglysSetup-x64-1.59.1.exe' -Lo VSCodeSetup-x64.exe
-curl 'https://github.com/vakaras/smauglys-ide/releases/download/1.59.1/SmauglysSetup-ia32-1.59.1.exe' -Lo VSCodeSetup-ia32.exe
+curl 'https://github.com/vakaras/smauglys-ide/releases/download/1.70.2/SmauglysSetup-x64-1.70.2.exe' -Lo VSCodeSetup-x64.exe
+curl 'https://github.com/vakaras/smauglys-ide/releases/download/1.70.2/SmauglysSetup-ia32-1.70.2.exe' -Lo VSCodeSetup-ia32.exe
 
 # Download VS Code extensions
 mkdir -p vscode_extensions
@@ -30,8 +30,8 @@ mkdir -p vscode_extensions
 try_download_package 'https://github.com/vakaras/vscode-language-pack-lt/releases/download/v-2021-09-04-1742/vscode-language-pack-lt.vsix' 'vscode_extensions/vakaras.vscode-language-pack-lt.vsix' '58a9ee12c5336658a38ccb40f1ec3e9a'
 #curl 'https://marketplace.visualstudio.com/_apis/public/gallery/publishers/ms-toolsai/vsextensions/jupyter/2021.8.1236758218/vspackage' --compressed -Lo vscode_extensions/ms-toolsai.jupyter.vsix
 # Download our translated version of the Python extension.
-try_download_package 'https://github.com/vakaras/vscode-python/releases/download/v-2021-09-04-1253/ms-python-v-2021-09-04-1253.vsix' 'vscode_extensions/ms-python.python.vsix' '1fb74bc6c7b91b7bb46a478befa21411'
-try_download_package 'https://marketplace.visualstudio.com/_apis/public/gallery/publishers/hediet/vsextensions/debug-visualizer/2.2.4/vspackage' 'vscode_extensions/hediet.debug-visualizer.vsix' 'b0265083182fcb1eb5aa7d1e0a4ac261'
+try_download_package 'https://github.com/vakaras/vscode-python/releases/download/v-2022-08-21-0945/ms-python-v-2022-08-21-0945.vsix' 'vscode_extensions/ms-python.python.vsix' 'bcc9d2629ea124b2128b7e558caa9b49'
+try_download_package 'https://marketplace.visualstudio.com/_apis/public/gallery/publishers/hediet/vsextensions/debug-visualizer/2.3.1/vspackage' 'vscode_extensions/hediet.debug-visualizer.vsix' '05e65a6666ed82521689fd177c0470fe'
 try_download_package 'https://github.com/gedas-luksas/quick-run-code/releases/download/0.0.3/quick-run-code-0.0.3.vsix' 'vscode_extensions/quick-run-code.vsix' '85ed1708da7913022756899e04bab986'
 #try_download_package 'https://github.com/gedas-luksas/vscode-code-runner/releases/download/0.11.6/code-runner-0.11.6.vsix' 'vscode_extensions/formulahendry.code-runner.vsix' 'f070e588b5fafaaffe0c088161c19b7b'
 
